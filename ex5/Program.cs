00fcbb2961957ea0126d45412de1ae01e3f34e4f
Евгений Мамадалиев <evgenijmamadaliev@MBP-Evgenij.Dlink>
2022-08-21 17:52:37 +0300

    ex5 masha

--- ex5/Program.cs
+++ ex5/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write(" enter name username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{      
+    Console.WriteLine("Ya Mashaaa");
+}
+else
+{
+       Console.Write("hello,   ");
+       Console.Write(username);
+}
